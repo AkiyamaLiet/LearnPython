786b3a125df95cb7d0d2487799fd3ca9c82d695f
AkiyamaLiet <1206814546@qq.com>
2017-08-29 00:06:59 +0800

    timer.py

--- timer.py
+++ timer.py
@@ -1,12 +1,21 @@
 # -*- coding:utf-8 -*-
 "用Python制作计时器"
 import time
-count = 0 
-a = input('time:') 
-b = a * 60 
-while (count < b):
-    ncount = b - count 
-    print ncount 
-    time.sleep(1)
-    count += 1 
-print 'done' 
+
+def Count():
+    count = 0 
+    min_input = input('time:') 
+    sec = min_input * 60 
+    while (count < sec):
+        sec_now = sec - count 
+        #    print sec_now 
+        #   输出提示
+        min_clk = sec_now / 60
+        sec_clk = sec_now % 60
+        print "%d:%d" %(min_clk, sec_clk)
+        time.sleep(1)
+        count += 1 
+    print 'done' 
+    
+if __name__ == '__main__':
+    Count()
