d0dc685b793834d9a35661aec25dfdde43a4d5d4
AkiyamaLiet <1206814546@qq.com>
2017-09-26 17:02:01 +0800

    整数划分问题

---
+++ Algorithm/整数划分问题.py
@@ -0,0 +1,14 @@
+def q(n, m):
+    if n<1 or m<1:
+        return 0
+    if n==1 or m==1:
+        return 1
+    if n<m:
+        return q(n, n)
+    if n==m:
+        return q(n, m-1) + 1
+    return q(n, m-1) + q(n-m, m)
+
+
+if __name__ == '__main__':
+    print q(6, 6)
